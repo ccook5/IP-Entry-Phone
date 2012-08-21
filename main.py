3548e1884ee866040536d36b9935a4335bc8c571
Chris Cook <ccook5@cgs.rbksch.org>
2012-08-21 23:45:19 +0000

    prevent the action from being repeated until the button has been release.

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/env python
+
+import RPi.GPIO as GPIO
+import time
+
+input_pin = 4
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(input_pin, GPIO.IN)
+
+while True:
+	if not GPIO.input(input_pin):
+		print("Button Pressed")
+		while not GPIO.input(input_pin):
+			time.sleep(0.1)
+		
