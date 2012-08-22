cb0416866a8f5f4d02340a1ea9ba579d5f63eb94
Chris Cook <ccook5@cgs.rbksch.org>
2012-08-22 22:52:40 +0000

    Added basic linphone startup code

--- main.py
+++ main.py
@@ -2,9 +2,15 @@
 
 import RPi.GPIO as GPIO
 import time
+import subprocess
 
 def setup_linphone():
 	print("Seting up Linphone");
+	subprocess.call(["linphonecsh", "init"])
+	subprocess.call(["linphonecsh", "register", 
+			"--host",       "192.168.0.106",
+			"--username",   "test",
+			"--password",   "password"])
 
 def make_call():
 	print("Button Pressed");
