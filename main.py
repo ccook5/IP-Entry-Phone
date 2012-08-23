e9817fd3d6d87898aa40d6ce4709b1700dc824b3
Chris Cook <ccook5@cgs.rbksch.org>
2012-08-23 15:43:14 +0000

    Changed some of the linphonecsh code, and added an unregister function to clean up at end

--- main.py
+++ main.py
@@ -3,23 +3,34 @@
 import RPi.GPIO as GPIO
 import time
 import subprocess
+import atexit
 
 def setup_linphone():
 	print("Seting up Linphone");
 	subprocess.call(["linphonecsh", "init"])
 	subprocess.call(["linphonecsh", "register", 
 			"--host",       "192.168.0.106",
-			"--username",   "test",
+			"--username",   "101",
 			"--password",   "password"])
+
+def unregister():
+	print("Unregistering")
+	subprocess.call(["linphonecsh", "unregister"])
 
 def make_call():
 	print("Button Pressed");
+	subprocess.call(["linphonecsh", "generic", 
+			'"call sip:100@192.168.0.100"'])
+
 
 input_pin = 4
 
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(input_pin, GPIO.IN)
+
+atexit.register(unregister)
+setup_linphone()
 
 while True:
 	if not GPIO.input(input_pin):
